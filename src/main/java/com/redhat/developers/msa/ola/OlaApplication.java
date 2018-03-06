3769ec14461fcbeb7e6c30e55c43c6c58d1949cd
juana nakfour <jnakfour@ovpn-124-48.rdu2.redhat.com>
2018-03-06 09:56:05 -0600

    Added logs

--- src/main/java/com/redhat/developers/msa/ola/OlaApplication.java
+++ src/main/java/com/redhat/developers/msa/ola/OlaApplication.java
@@ -25,7 +25,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class OlaApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OlaApplication.class);
+
+    @PostConstruct
+    public void logSomething() {
+        LOGGER.trace("doStuff needed more information - {}", value);
+        LOGGER.debug("doStuff needed to debug - {}", value);
+        LOGGER.info("doStuff took input - {}", value);
+        LOGGER.warn("doStuff needed to warn - {}", value);
+        LOGGER.error("doStuff encountered an error with value - {}", value);
+    }
     public static void main(String[] args) {
+
         SpringApplication.run(OlaApplication.class, args);
     }
 
