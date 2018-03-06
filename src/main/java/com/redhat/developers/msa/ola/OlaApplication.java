68702f8ac08b11a234ea25b2430015a8f9993e57
juana nakfour <jnakfour@ovpn-124-48.rdu2.redhat.com>
2018-03-06 10:28:52 -0600

    Added colored logs

--- src/main/java/com/redhat/developers/msa/ola/OlaApplication.java
+++ src/main/java/com/redhat/developers/msa/ola/OlaApplication.java
@@ -33,13 +33,18 @@ public class OlaApplication {
 
     @PostConstruct
     public void logSomething() {
-        LOGGER.trace("doStuff needed more information - {}", value);
-        LOGGER.debug("doStuff needed to debug - {}", value);
-        LOGGER.info("doStuff took input - {}", value);
-        LOGGER.warn("doStuff needed to warn - {}", value);
-        LOGGER.error("doStuff encountered an error with value - {}", value);
+        LOGGER.trace("doStuff needed more information - {}");
+        LOGGER.debug("doStuff needed to debug - {}");
+        LOGGER.info("doStuff took input - {}");
+        LOGGER.warn("doStuff needed to warn - {}");
+        LOGGER.error("doStuff encountered an error with value - {}");
     }
     public static void main(String[] args) {
+        LOGGER.trace("ddddddddddoStuff needed more information - {}");
+        LOGGER.debug("ddddddddddoStuff needed to debug - {}");
+        LOGGER.info("ddddddddddoStuff took input - {}");
+        LOGGER.warn("dddddddddoStuff needed to warn - {}");
+        LOGGER.error("dddddddddoStuff encountered an error with value - {}");
 
         SpringApplication.run(OlaApplication.class, args);
     }
